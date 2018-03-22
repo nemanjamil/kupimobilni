29b523ef4ee2e9dd196b97f899c0df1da77b95f2
nemanjamil <nemanjamil@gmail.com>
2018-03-22 21:15:09 +0100

    Dodaj Slike

---
+++ commonAgro.php
@@ -0,0 +1,20 @@
+<?php
+
+define('SIFRAZACOOKIE', 'klasterica');
+define('GLAVNIMAIL', 'xxxxx@gmail.com');
+//define('KATEGORIJESAJT', '1');
+define('PASSMAIL', 'ohtxjjweaticabpv');
+define('FROMNAME', 'Masine i Alati');
+
+
+define('KATALATIMASINE', 168);
+define('KATPOTROSNIMATERIJAL', 170);
+define('KATSRAFOVI', 171);
+
+define('ELASTICINDEX', "devshone"); // ovo je da prikaze 50 artikala po strani
+define('ELASTICGRUPE', "artikli"); // ovo je da prikaze 50 artikala po strani
+
+
+
+
+
