5b63de482060c740cf0638b89f2b623c62a7aecf
nemanjamil <nemanjamil@gmail.com>
2018-03-25 20:00:41 +0200

    DCROOT

--- vezafullCron.php
+++ vezafullCron.php
@@ -1,5 +1,6 @@
 <?php
 require_once($documentroot.'/include/MysqliDb.php');
+require_once($documentroot.'/post_get.php');
 require_once($documentroot.'/include/vezica.php');
 $common = new common($db);
 $kategorije = new kategorije($db);
