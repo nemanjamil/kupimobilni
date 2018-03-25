0297c763d377daf9779bc37569455cc01aa6aad1
nemanjamil <nemanjamil@gmail.com>
2018-03-25 20:31:16 +0200

    downloadKategorija

--- vezafullCron.php
+++ vezafullCron.php
@@ -1,4 +1,6 @@
 <?php
+define('URLCALCSERVICE', 'http://10.8.0.6/CalculusWebService/CalculusWebService.asmx/');
+
 require_once($documentroot.'/include/MysqliDb.php');
 require_once($documentroot.'/post_get.php');
 require_once($documentroot.'/include/vezica.php');
