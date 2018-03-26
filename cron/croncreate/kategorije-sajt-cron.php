9b1f3032e39c8cd03ddcc00a973e4b2a24b42755
nikolamarkovic <markovicnikola@outlook.com>
2018-03-26 12:27:25 +0200

    Cron za kategorije

---
+++ cron/croncreate/kategorije-sajt-cron.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Nikola Markovic
+ * Date: 26.3.2018.
+ * Time: 11:51
+ */
+
+/*
+ * 6726 - Auto - 02.
+ * 6728 - Mobilni - 09.
+ * 6727 - Laptop - 07.
+ * 5046 - Kamere - 06.
+ * 6730 - Racunar - 14.
+ * */
+
+
+$cols = Array("K.KategorijaArtikalaId");
+$db->where('K.KategorijaArtikalaSifra', Array('02.', '09.','07.', '14.'), 'IN');
+$tagoviArtupit = $db->get('kategorijeartikala K', null, $cols);
+if ($tagoviArtupit) {
+    $tagoviArt = $common->array_2_csv_sa_dodatkomnavodnika($tagoviArtupit);
+}
+
+
+define('KATEGORIJESAJTCRON', $tagoviArt);
